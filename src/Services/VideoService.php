d0dc84109aefb472f525641e95c2f8233d5f967d
janpaulalmanoche <44863381+janpaulalmanoche@users.noreply.github.com>
2024-08-20 09:04:24 +0800

    Initial commit

---
+++ src/Services/VideoService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Vendor\Veeroll\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Models\Video;
+use App\Models\Language;
+use App\Models\TextEffect;
+use App\Models\MusicLibrary;
+use App\Models\VideoEffect;
+use App\Http\Resources\VideoResource;
+
+class VideoService
+{
+
+    protected $httpClientService;
+    public function __construct(HttpClientService $httpClientService)
+    {
+        $this->httpClientService = $httpClientService;
+    }
+
+    public function expressMode($request)
+    {
+        $data = [
+            'json' => $request->all(),
+            'headers' => [
+                'secret' => env('VEEROLL_SECRET', ''),
+                'api_key' => env('VEEROLL_API_KEY', ''),
+            ],
+        ];
+
+        return $this->httpClientService->makeRequest('POST', 'video-express-mode', $data);
+    }
+
+    public function stepByStep($request)
+    {
+        $data = [
+            'json' => $request->all(),
+            'headers' => [
+                'secret' => env('VEEROLL_SECRET', ''),
+                'api_key' => env('VEEROLL_API_KEY', ''),
+            ],
+        ];
+
+        return $this->httpClientService->makeRequest('POST', 'video', $data);
+    }
+}
